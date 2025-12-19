8fb27723c1143783ac2d6a100d9ead1095b283e0
Shvetank Dattatreya <hello.shvetank@gmail.com>
2025-12-19 09:47:48 +0530

    Improved functionalities slightly

---
+++ LOG.md
@@ -0,0 +1,4 @@
+# Version 0.2.0
+
+- Added the option to either enter the answer or the option number for the correct answer
+- Added the function to show if the answer is wrong.
--- src/poppy/poppy.py
+++ src/poppy/poppy.py
@@ -1,10 +1,10 @@
 import html
-from poppy import fetch_questions
+from poppy import fetch_response
 
 
 
 def main():
-    question_set = fetch_questions.fetch_response()
+    question_set = fetch_response.fetch_response()
     points = 0
 
     for index, each_question in enumerate(question_set):
@@ -22,9 +22,18 @@ def main():
     
         stringo = input("Enter Answer or Option: ")
 
-        if int(stringo) == all_answers.index(correct_answer) + 1:
-            print("You got it right!")
-            points += 3
+        if len(stringo.strip()) < 2:
+            if int(stringo) == all_answers.index(correct_answer) + 1:
+                print("You got it right!")
+                points += 3
+            else:
+                print("That is not right!")
+        else:
+            if stringo == correct_answer:
+                print("You got it right!")
+                points += 3
+            else:
+                print("That is not right!")
 
 
     print(f"You got a total of {points} points.")
